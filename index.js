d09d651b0860289992407865a0b48effe56a8001
Michael Olmos Trujillo <molmost@unal.edu.co>
2021-01-27 02:29:44 -0500

    estableciendo conexión entre el frontend y el backend

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     fetch('http://localhost:5000/getAll')
-    .then(response => response.json())
-    .then(data => console.log(data));
-    loadHTMLTable([]);
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']));
+
 });
 
-function loadHTMLTable(data){
-    const table = document.querySelector('table tbody');
-let tableHtml = "";
+const addBoton = document.getElementById('add-name-btn');
 
-if(data.length === 0){
-    table.innerHTML = "<tr> <td class = 'no-data' colspan = '5'> No Data </td> </tr>"
-}
+addBoton.addEventListener('click', () => {
+    const nameInput = document.getElementById('name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+
+    fetch('http://localhost:5000/insert', {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                name: name
+            })
+        })
+        .then(response => response.json())
+        .then(data => );
+})
+
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+    let tableHtml = "";
+
+    console.log(data)
+
+    if (data.length === 0) {
+        table.innerHTML = "<tr> <td class = 'no-data' colspan = '5'> No Data </td> </tr>"
+    }
 
 }
